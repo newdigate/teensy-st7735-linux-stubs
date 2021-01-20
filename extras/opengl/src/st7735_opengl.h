ccde74d348f6149ed82540f75a2636bdb5902340
newdigate <nicnewdigate@gmail.com>
2021-01-20 16:36:52 +0000

    fix build dependencies

--- extras/opengl/src/st7735_opengl.h
+++ extras/opengl/src/st7735_opengl.h
@@ -8,7 +8,11 @@
 #include <iostream>
 #include "../../../src/ST7735_t3.h"
 #include <GLFW/glfw3.h>
+#ifdef __APPLE__
 #include <OpenGL/gl3.h>
+#else
+#include <GL/gl.h>
+#endif
 static const char* vertexShaderCode = R"glsl(
 #version 330 core
 layout (location = 0) in vec3 aPos;
